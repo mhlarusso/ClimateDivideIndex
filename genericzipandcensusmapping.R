7d3ce795d091e93c7811d1a4c86d40ec8a4eb550
Michelle Koop <mjpkoop@gmail.com>
2020-01-10 14:23:34 -0500

    Cleaned up a bit and added commentary

--- genericzipandcensusmapping.R
+++ genericzipandcensusmapping.R
@@ -1,25 +1,27 @@
 #Climate Divide Project - DST 2019-2020
 
-#Authors: Michelle Koop, Nicole Carr, Jordan Lewis, Mallory LaRusso
+# Authors: Michelle Koop, Nicole Carr, Jordan Lewis, Mallory LaRusso
+
+# Instructions for user to do to run program:
+  # Change the city names in cityname variable (line 20)
+  # Get the zip codes for each city from the supplementary excel document, 
+    # just copy and paste into the "c()" (line 23)
+  # Change the length of num_cols by looking at tract matrix (making num_cols big right now)
 
 
-#different things to do for each city:
-# change the city names in cityname variable
-# get the zip codes for each city
-# change the length of num_cols by looking at tract matrix (making num_cols big right now)
-
-
-#installing necessary packages
+# Installing necessary packages
 library("readxl")
 
 
-#grabbing the zip code and census tract data
+# Importing the zip code and census tract data from https://www.huduser.gov/portal/datasets/usps_crosswalk.html
+# Select the crosswalk type to be "ZIP-TRACT"
+# Select Data Year and Quarter to be "3rd Quarter 2019"
 zipandcensus <- read_excel("ZIP_TRACT_092019.xlsx")
 num_tracts <- dim(zipandcensus)[1]
 cityname <- "Toledo"
 
-#get all of the codes in city from https://www.zip-codes.com/city
-zip_codes<- c(, 43601
+# Retrieve all of the zip codes in a city from https://www.zip-codes.com/city and turn them into a vector
+zip_codes<- c(  43601
               , 43603
               , 43604
               , 43605
@@ -50,12 +52,14 @@ zip_codes<- c(, 43601
               , 43682
               , 43697
               , 43699)
-
+# Convert the current type to characters
 city <- as.character(zip_codes)
-num_zips <- length(city) 
+
+# Obtain the number of zipcodes or length of the vector for the chosen city.
+num_zips <- length(city)
 
 
-#for loop to make a list of lists of each zip code and the census tract it maps to
+# Use a for loop to make a list of lists of each zip code and the census tract it maps to
 zip_list <- list()
 for (i in 1:num_zips){
   tractlist <- list()
@@ -73,10 +77,8 @@ for (i in 1:num_zips){
 zip_list
 
 
-#create matrix out of zip_list (which is a list of lists) in order to create a dataframe to write to a csv
-
+# Create matrix out of zip_list (which is a list of lists) in order to create a dataframe to write to a csv
 num_cols = 40
-
 tractmatrix <- matrix(ncol = num_cols, nrow = num_zips) 
 
 for (i in 1:num_zips){
@@ -84,16 +86,17 @@ for (i in 1:num_zips){
     tractmatrix[i,j] <- zip_list[[i]][[j]]
   }
 }
-tractmatrix #look at tract matrix to find out how many columns you need for num_cols. try 50 to start with, narrow down to the longest list of observations
+tractmatrix  # Look at tract matrix to find out how many columns you need for num_cols. 
+             # Try 50 to start with, narrow down to the longest list of observations
 
 
 tractdataframe <- data.frame(tractmatrix)
 tractdataframe
 
-#cross refrence csv of every zip code in the city and all the census tracts in it
+# Cross refrence csv of every zip code in the city and all the census tracts in it
 write.csv(tractdataframe, paste(cityname,"Cross Reference.csv"))
 
-#get a list of every census tract in city
+# Get a list of every census tract in city
 tracts <- c()
 count = 0
 for (i in 1:num_zips){
@@ -104,27 +107,28 @@ for (i in 1:num_zips){
 }
 
 
-#delete duplicates
+# Delete duplicates
 uniquetracts <- unique(tracts)
 
-#write to csv
+# Write the census tracts to a .csv
 write.csv(uniquetracts, paste(cityname, "Census Tracts.csv"))
 
-#need to find the zip codes from the list of city zip codes that have census tracts
-
+# Collate the the city zip codes that have at least one census tract to their respective tracts
 columntwo <- tractmatrix[, 2]
 Zip_codes_with_tracts1 <- c()
 count = 0
 for (i in 1:num_zips){
   count = count + 1
-  if (!is.na(columntwo[i])) {
+  if (!is.na(columntwo[i])) { # Only uses zip codes with at least one or more tract(s)
     Zip_codes_with_tracts1[count] <- zip_codes[i]
   }
 }
 
+# Creates offical zip code and tract list by deleting tracts that map to more than one zip code 
+# from zip_codes_with_tracts1
 Zip_codes_with_tracts <- unique(Zip_codes_with_tracts1)
 Zip_codes_with_tracts
 
-#write a csv of every zip code in city
+# Writes a .csv of every zip code in city
 write.csv(Zip_codes_with_tracts, paste(cityname, "Zip Codes.csv"))
 
